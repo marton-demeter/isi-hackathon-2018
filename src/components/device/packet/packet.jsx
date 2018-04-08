c051ff7e9b0343f11b437dea46e38ba0312f2966
Marton Demeter <marton1029@gmail.com>
2018-04-08 10:17:46 -0700

    added restyling for mobile

--- src/components/device/packet/packet.jsx
+++ src/components/device/packet/packet.jsx
@@ -34,54 +34,118 @@ class Packet extends React.Component {
         className={c(s.packet,this.state.allow?s.allow:s.block)}
         onClick={e => this.handlePacketClick(e)}
         >
-        <table>
-          <tbody>
-            <tr>
-              <th>Date: </th>
-              <td> { this.props.date }</td>
-              <th>Destination MAC Address: </th>
-              <td> { this.props.dst_mac }</td>
-            </tr>
-            <tr>
-              <th>Time: </th>
-              <td> { this.props.time }</td>
-              <th>Destination IP Address: </th>
-              <td> { this.props.dst_ip }</td>
-            </tr>
-            <tr>
-              <th>Source MAC Addreess: </th>
-              <td> { this.props.src_mac }</td>
-              <th>Destination Port: </th>
-              <td> { this.props.dst_port }</td>
-            </tr>
-            <tr>
-              <th>Source IP Address: </th>
-              <td> { this.props.src_ip }</td>
-              <th>Protocol: </th>
-              <td> { this.props.protocol }</td>
-            </tr>
-            <tr>
-              <th>Source Port: </th>
-              <td> { this.props.src_port }</td>
-              <th>Good: </th>
-              <td> { this.props.good }</td>
-            </tr>
-            <tr className={this.props.rule?null:s.norule}>
-              <th> Rule: </th>
-              <td colSpan='3'> { this.props.rule || 'Rule Description' } </td>
-            </tr>
-          </tbody>
-        </table>
-        <div className={s.buttons}>
-          <div 
-            className={s.a} 
-            onClick={e => this.handleButtonClick(e,1)}>
-            <i className={c(s.fa,s['fa-check'])} />
+        <div className={s.desktop}>
+          <table>
+            <tbody>
+              <tr>
+                <th>Date: </th>
+                <td> { this.props.date }</td>
+                <th>Destination MAC Address: </th>
+                <td> { this.props.dst_mac }</td>
+              </tr>
+              <tr>
+                <th>Time: </th>
+                <td> { this.props.time }</td>
+                <th>Destination IP Address: </th>
+                <td> { this.props.dst_ip }</td>
+              </tr>
+              <tr>
+                <th>Source MAC Addreess: </th>
+                <td> { this.props.src_mac }</td>
+                <th>Destination Port: </th>
+                <td> { this.props.dst_port }</td>
+              </tr>
+              <tr>
+                <th>Source IP Address: </th>
+                <td> { this.props.src_ip }</td>
+                <th>Protocol: </th>
+                <td> { this.props.protocol }</td>
+              </tr>
+              <tr>
+                <th>Source Port: </th>
+                <td> { this.props.src_port }</td>
+                <th>Good: </th>
+                <td> { this.props.good }</td>
+              </tr>
+              <tr className={this.props.rule?null:s.norule}>
+                <th> Rule: </th>
+                <td colSpan='3'> { this.props.rule || 'Rule Description' } </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className={s.buttons}>
+            <div 
+              className={s.a} 
+              onClick={e => this.handleButtonClick(e,1)}>
+              <i className={c(s.fa,s['fa-check'])} />
+            </div>
+            <div 
+              className={s.reject} 
+              onClick={e => this.handleButtonClick(e,0)}>
+              <i className={c(s.fa,s['fa-times'])} />
+            </div>
           </div>
-          <div 
-            className={s.reject} 
-            onClick={e => this.handleButtonClick(e,0)}>
-            <i className={c(s.fa,s['fa-times'])} />
+        </div>
+        <div className={s.mobile}>
+          <table>
+            <tbody>
+              <tr>
+                <th>Date: </th>
+                <td> { this.props.date }</td>
+              </tr>
+              <tr>
+                <th>Time: </th>
+                <td> { this.props.time }</td>
+              </tr>
+              <tr>
+                <th>Source MAC Addreess: </th>
+                <td> { this.props.src_mac }</td>
+              </tr>
+              <tr>
+                <th>Source IP Address: </th>
+                <td> { this.props.src_ip }</td>
+              </tr>
+              <tr>
+                <th>Source Port: </th>
+                <td> { this.props.src_port }</td>
+              </tr>
+              <tr className={this.props.rule?null:s.norule}>
+                <th> Rule: </th>
+                <td colSpan='3'> { this.props.rule || 'Rule Description' } </td>
+              </tr>
+              <tr>
+                <th>Destination MAC Address: </th>
+                <td> { this.props.dst_mac }</td>
+              </tr>
+              <tr>
+                <th>Destination IP Address: </th>
+                <td> { this.props.dst_ip }</td>
+              </tr>
+              <tr>
+                <th>Destination Port: </th>
+                <td> { this.props.dst_port }</td>
+              </tr>
+              <tr>
+                <th>Protocol: </th>
+                <td> { this.props.protocol }</td>
+              </tr>
+              <tr>
+                <th>Good: </th>
+                <td> { this.props.good }</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className={s.buttons}>
+            <div 
+              className={s.a} 
+              onClick={e => this.handleButtonClick(e,1)}>
+              <i className={c(s.fa,s['fa-check'])} />
+            </div>
+            <div 
+              className={s.reject} 
+              onClick={e => this.handleButtonClick(e,0)}>
+              <i className={c(s.fa,s['fa-times'])} />
+            </div>
           </div>
         </div>
       </div>
